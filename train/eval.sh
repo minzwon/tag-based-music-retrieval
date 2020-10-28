7b8af46f90c6f0d2e99bcc2009d62ba828513f10
Minz Won <minzwon@Minzui-MacBookPro.local>
2020-10-28 15:49:04 +0900

    examples

--- train/eval.sh
+++ train/eval.sh
@@ -3,7 +3,7 @@ python -u main.py \
 --mode 'TEST' \
 --input_type 'cf' \
 --w2v_type 'google' \
---data_path '/home/minz.s.won/data/pandora' \
---model_load_path './checkpoints/b-w-sub-cf-google/epoch=193.ckpt' \
---neptune_project 'minzwon/pandora' \
---neptune_api_key 'eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vdWkubmVwdHVuZS5haSIsImFwaV91cmwiOiJodHRwczovL3VpLm5lcHR1bmUuYWkiLCJhcGlfa2V5IjoiZWJkZGQzNTctNTllZC00ZDg0LTlkZTMtMWZkYThjZjRkMDQwIn0=' \
+--data_path 'YOUR_DATA_PATH' \
+--model_load_path './checkpoints/b-w-cf-google/epoch=193.ckpt' \
+--neptune_project 'YOUR_NEPTUNE_ID/YOUR_PROJECT_NAME' \
+--neptune_api_key 'YOUR_NEPTUNE_API_KEY' \
